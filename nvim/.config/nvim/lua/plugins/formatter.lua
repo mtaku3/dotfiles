9f0c6468d93bed1cb89367e838babfe66264d9af
mtaku3 <m.taku3.1222@gmail.com>
2023-11-27 13:20:00 +0900

    feat: add formatter to nvim

---
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "mhartington/formatter.nvim",
+    config = function(_, opts)
+      require("formatter").setup({
+        lua = {
+          require("formatter.filetypes.lua").stylua,
+        },
+      })
+      vim.keymap.set({ "n", "v" }, "<leader>f", ":Format<CR>", { desc = "Format current buffer" })
+    end,
+  },
+}
