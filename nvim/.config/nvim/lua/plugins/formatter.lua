27792361aaa9a94234d6b1e1dc5415832e4525c2
mtaku3 <m.taku3.1222@gmail.com>
2023-11-28 16:22:22 +0900

    revert(nvim): do not use biome formatter

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -13,21 +13,6 @@ return {
 						require("formatter.filetypes.python").isort,
 						require("formatter.filetypes.python").black,
 					},
-					javascript = {
-						require("formatter.filetypes.javascript").biome,
-					},
-					javascriptreact = {
-						require("formatter.filetypes.javascriptreact").biome,
-					},
-					typescript = {
-						require("formatter.filetypes.typescript").biome,
-					},
-					typescriptreact = {
-						require("formatter.filetypes.typescriptreact").biome,
-					},
-					json = {
-						require("formatter.filetypes.json").biome,
-					},
 				},
 			})
 			vim.keymap.set("n", "<leader>f", ":Format<CR>", { desc = "Format current buffer" })
