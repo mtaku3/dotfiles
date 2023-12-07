78c744e490a884b018c71f6547b67c77c49e07b3
mtaku3 <m.taku3.1222@gmail.com>
2023-12-07 16:40:00 +0900

    feat(nvim): Copilot

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -14,9 +14,23 @@ return {
 					end,
 				},
 			},
+			{
+				"zbirenbaum/copilot-cmp",
+				dependencies = { "zbirenbaum/copilot.lua" },
+				config = true,
+			},
 		},
 		lazy = true,
 		config = function()
+			local has_words_before = function()
+				if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+					return false
+				end
+				local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+			end
+
 			local cmp = require("cmp")
 			cmp.setup({
 				snippet = {
@@ -38,7 +52,7 @@ return {
 						select = true,
 					}),
 					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
+						if cmp.visible() and has_words_before then
 							cmp.select_next_item()
 						elseif require("luasnip").expand_or_locally_jumpable() then
 							require("luasnip").expand_or_jump()
@@ -57,6 +71,7 @@ return {
 					end, { "i", "s" }),
 				}),
 				sources = {
+					{ name = "copilot" },
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 				},
