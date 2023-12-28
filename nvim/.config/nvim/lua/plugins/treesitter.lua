664709081756581f13fd3263fec3fabb3e1ea69c
mtaku3 <m.taku3.1222@gmail.com>
2023-12-28 10:01:41 +0900

    feat: enable auto install for treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,11 +9,11 @@ return {
 			-- Defer Treesitter setup after first render to improve startup time of "nvim {filename}"
 			vim.defer_fn(function()
 				require("nvim-treesitter.configs").setup({
-					-- Add languages to be installed here that you want installed for treesitter
-					ensure_installed = { "lua" },
+					-- -- Add languages to be installed here that you want installed for treesitter
+					-- ensure_installed = { "lua" },
 
 					-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-					auto_install = false,
+					auto_install = true,
 
 					highlight = { enable = true },
 					indent = { enable = true },
